75d005a43065891386b0304831cb4e9e89110c11
Trista Shan <trista8703@hotmail.com>
2024-04-22 21:51:11 +0300

    card

--- src/components/valueCards/ValuePieces.jsx
+++ src/components/valueCards/ValuePieces.jsx
@@ -1,3 +1,12 @@
-export const ValuePieces = ()=> {
-    
+export const ValuePieces = ({image, alt, heading, valueContent, readMore})=> {
+    return (
+        <div>
+            <img src={image} alt={alt} />
+            <div>
+                <h5>{heading}</h5>
+                <p>{valueContent}</p>
+            </div>
+            <p>{readMore}</p>
+        </div>
+    )
 }
