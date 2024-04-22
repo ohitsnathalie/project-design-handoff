1a47c57cdbe49bf677e7dc511a8bed1f472462a1
Trista Shan <trista8703@hotmail.com>
2024-04-22 18:39:50 +0300

    class card done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     screens: {
       sm: "320px",
       md: "744px",
-      lg: "1024px",
+      lg: "1440px",
     },
     colors: {
       yellow: "#F6E53C",
@@ -23,6 +23,7 @@ export default {
       "2xl": ["35px", { fontWeight: "700" }],
       "3xl": ["40px", { fontWeight: "700" }],
       "4xl": ["55px", { fontWeight: "700" }],
+      "5xl": ["25px", {fontWeight: "400"}]
     },
     spacing: {
       1: "16px",
@@ -41,6 +42,10 @@ export default {
         "hero-image": "url('src/assets/woman-dancing.jpg')",
       },
     },
+    boxShadow: {
+      'sm': '3px 3px rgba(0, 0, 0, 0.5)',
+      'base': '5px 5px rgba(0, 0, 0, 0.5)',
+    }
   },
   plugins: [],
 }
